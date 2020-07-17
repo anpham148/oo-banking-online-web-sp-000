b30ff5ae98e97479dd098a0e70029d597a5a1f7d
An Pham <anpham148@gmail.com>
2020-07-17 10:57:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending" && @sender.balance > @amount && self.valid?
+    if @status == "pending" && @sender.balance > @amount && @sender.valid?
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
