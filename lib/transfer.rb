d1750d3c4d4ed7d735bbec4460104111eaae1362
An Pham <anpham148@gmail.com>
2020-07-17 11:08:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,14 +14,14 @@ class Transfer
 
   def execute_transaction
     if @status == "pending"
-      if @sender.balance >= self.amount && self.valid?
-        @sender.balance = @sender.balance - @amount
-        @receiver.balance = @receiver.balance + @amount
-        @status = "complete"
-      else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+        if @sender.balance >= self.amount && self.valid?
+          @sender.balance = @sender.balance - @amount
+          @receiver.balance = @receiver.balance + @amount
+          @status = "complete"
+        else
+          @status = "rejected"
+          "Transaction rejected. Please check your account balance."
+        end
     end
 
   end
