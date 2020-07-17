2e70b44fb664dd1399cf0ba7a1419336818b9b40
An Pham <anpham148@gmail.com>
2020-07-17 10:54:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-    elsif @status == "pending" && @sender.balance < @amount
+    elsif @status == "pending" && @sender.balance < @amount && !self.valid?
       "Transaction rejected. Please check your account balance."
     end
 
